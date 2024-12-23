8740a2e55a79f6ae4b9a402fc5c666f1020b9237
Austin L Mayes <me@austinlm.me>
2024-12-22 22:46:37 -0600

    More multi-part utils

--- components/multi_part.rb
+++ components/multi_part.rb
@@ -42,10 +42,19 @@ namespace :mp do
   task merge_down: :before do |task, args|
     ensure_on_multi_part_branch
     max = args.extras[0].nil? ? Float::INFINITY : args.extras[0].to_i
+    try_build = args.extras[1] == "build"
     branches = stage_branches(:up)
+    if try_build
+      system "./gradlew", "classes"
+      error "Build failed on branch #{@current}" unless $?.success?
+    end
     branches.each do |branch|
       if to_branch(branch, strategy: :rebase)
         system "git", "checkout", branch
+        if try_build
+          system "./gradlew", "classes"
+          error "Build failed on branch #{branch}" unless $?.success?
+        end
       else
         error "merge failed"
       end
@@ -78,7 +87,37 @@ namespace :mp do
     base = match[1]
     num = match[2].to_i
     new_branch = "#{base}-stage-#{num + 1}"
-    error "Branch #{new_branch} already exists!" if Git.branch_exists(new_branch)
+    error "Branch #{new_branch} already exists!" if Git.branch_exists("austin/" + new_branch)
+    make_branch new_branch, @current
+  end
+
+  desc "Make a new stage branch at the next index and move all future branches up one"
+  task new_part_and_move: :before do |task, args|
+    ensure_on_multi_part_branch
+    match = Git.current_branch.match(MULTI_PART_PATTERN)
+    base = match[1]
+    num = match[2].to_i
+    new_branch = "#{base}-stage-#{num + 1}"
+    if Git.branch_exists("austin/" + new_branch)
+      system "git", "stash"
+      info "Branch #{new_branch} already exists! Moving all future branches up one"
+      # move all future branches up one
+      branches = stage_branches(:up)
+      info "Going to move #{branches.count} branches up one"
+      branches.reverse.each do |branch|
+        system "git", "checkout", branch
+        new_num = branch.match(MULTI_PART_PATTERN)[2].to_i + 1
+        new_branch = "#{base}-stage-#{new_num}"
+        system "git", "branch", "-m", new_branch
+        info "Renamed #{branch} to #{new_branch}"
+      end
+      info "Renamed all branches"
+      system "git", "stash", "pop"
+      system "git", "checkout", @current
+      new_branch = "#{base}-stage-#{num + 1}"
+    else
+      info "Branch #{new_branch} does not exist! Creating it"
+    end
     make_branch new_branch, @current
   end
 
@@ -109,6 +148,39 @@ namespace :mp do
     system "git", "stash"
     system "git", "checkout", branches.first
     system "git", "stash", "pop"
+  end
+
+  desc "Go down to the last stage"
+  task last_part: :before do |task, args|
+    ensure_on_multi_part_branch
+    branches = stage_branches(:up)
+    error "No branches to go down to!" if branches.empty?
+    system "git", "stash"
+    system "git", "checkout", branches.last
+    system "git", "stash", "pop"
+  end
+
+  desc "Go up to the first stage"
+  task first_part: :before do |task, args|
+    ensure_on_multi_part_branch
+    branches = stage_branches(:down)
+    error "No branches to go up to!" if branches.empty?
+    system "git", "stash"
+    system "git", "checkout", branches.last
+    system "git", "stash", "pop"
+  end
+
+  desc "Run ./gradlew classes on each stage branch amd stop on failure"
+  task test_build: :before do |task, args|
+    ensure_on_multi_part_branch
+    branches = stage_branches(:up)
+    error "No branches to test build!" if branches.empty?
+    branches.each do |branch|
+      system "git", "checkout", branch
+      system "./gradlew", "classes"
+      error "Build failed on branch #{branch}" unless $?.success?
+    end
+    system "git", "checkout", @current
   end
 
   desc "Create PR for current stage branch based on previous stage branch"
